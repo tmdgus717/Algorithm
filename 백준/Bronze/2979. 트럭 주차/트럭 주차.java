3725253206b50cd45f981c42b981a55e5d940f5b
이승현 <67327887+tmdgus717@users.noreply.github.com>
2024-08-16 14:56:24 +0900

    [Bronze II] Title: 트럭 주차, Time: 100 ms, Memory: 14068 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.java
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.java
@@ -1,39 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) throws IOException{
+public class Main {//2979
+    static int[] time = new int [101];
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        //요금
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
 
-        int priceSize = 3;
-        int[] price = new int[priceSize];
-        for (int i = 0; i < priceSize; i++) {
-            //br은 스트링이므로
-            price[i] = Integer.parseInt(st.nextToken());
-        }
-
-        //입력으로 주어지는 시간은 1과 100사이 이다.
-        int[] time = new int[101];
-        // 시간은 이상 ~ 미만
-        for (int i = 0; i < 3; i++) {
+        for (int t = 0; t < 3; t++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            for (int j = start; j <  end; j++) {
-                time[j]++;
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            for (int i = n; i < m; i++) {
+                time[i]++;
             }
         }
 
-        int answer = 0;
-        for (int i = 1; i < 101; i++) {
-            if(time[i] == 1) answer += price[0];
-            else if (time[i] == 2) answer += price[1]*2;
-            else if (time[i] == 3) answer += price[2]*3;
+        int ans = 0;
+        for (int carNum : time) {
+            if(carNum == 1){
+                ans += a;
+            }
+            if (carNum == 2){
+                ans += (b * 2);
+            }
+            if (carNum == 3) {
+                ans += (c * 3);
+            }
         }
-
-        System.out.println(answer);
+        System.out.println(ans);
     }
 }
