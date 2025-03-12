fd825b258bba8f21c95769875976e713ab8c69d8
Seunghyun Lee <67327887+tmdgus717@users.noreply.github.com>
2025-03-12 21:43:33 +0900

    [Silver III] Title: 수열, Time: 692 ms, Memory: 114448 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,28 +1,23 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args)  {
+
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        //n = 전체 날짜의 수, k = 합을 구하기 위한 연속적인 날짜의 수
         int n = sc.nextInt();
         int k = sc.nextInt();
-        // 배열 생성
+        //최소값 설정
+        int max = -100 * 100000;
+
+        //구간합 문제!!
         int[] arr = new int[n+1];
-        //여기에 누적합으로 풀어야 할 것 같은 느낌이 들기는 하는데
-        for (int i = 1; i <= n; i++) {
-
-            int num = sc.nextInt();
-            arr[i] = arr[i - 1] + num;
+        for (int i = 1; i < n+1; i++) {
+            arr[i] = sc.nextInt() + arr[i-1]; //이전값과 현재값을 누적
         }
-
-        int max = 0;
-        for (int i = k; i <= n; i++) {
-            int tmp = arr[i] - arr[i - k];
-            if (i == k) {// max 값이 0보다 작은 경우가 있을 수 있다!! (반례)
-                max = tmp;
-                continue;
-            }
-            if (tmp > max) max = tmp;
+        for (int i = k; i < n + 1; i++) {
+            int temp = arr[i] - arr[i - k];
+            max = Math.max(max,temp);
         }
 
         System.out.println(max);
