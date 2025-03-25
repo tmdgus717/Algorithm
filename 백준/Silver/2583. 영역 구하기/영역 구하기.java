791419cf4ab987bb380a8c480b8d8106924bfcbe
SeungHyun <67327887+tmdgus717@users.noreply.github.com>
2025-03-25 15:01:04 +0900

    [Silver I] Title: 영역 구하기, Time: 124 ms, Memory: 16248 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -1,66 +1,76 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+//package chapter02;
 
-public class Main {//2583
+import java.io.*;
+import java.util.*;
+public class Main {
+
+    static int[] dy = {0, -1, 0, 1};
+    static int[] dx = {1, 0, -1, 0};
     static int[][] arr;
     static int[][] vis;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        int m = Integer.parseInt(st.nextToken()); //세로
-        int n = Integer.parseInt(st.nextToken()); //가로
-        int k = Integer.parseInt(st.nextToken());
-        arr = new int[m][n];
-        vis = new int[m][n];
-        while (k-- > 0) {
-            //직사각형 색칠하기
-            st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken()); //Y
+        int N = Integer.parseInt(st.nextToken()); //X
+        int K = Integer.parseInt(st.nextToken()); //직사각형 수
 
-            for (int i = y1; i < y2; i++) {
-                for (int j = x1; j < x2; j++) {
-                    arr[i][j] = 1;
-                }
-            }
-        }//while
-        int ans = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if(arr[i][j] == 0 && vis[i][j] == 0){
-                    dfs(i, j, m, n);
-                    ans++;
-                    arrayList.add(size);
-                    size=0;
+        arr = new int[M][N];
+        vis = new int[M][N];
+
+        //지도 만들기
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int ldx = Integer.parseInt(st.nextToken());
+            int ldy = Integer.parseInt(st.nextToken());
+            int rux = Integer.parseInt(st.nextToken());
+            int ruy = Integer.parseInt(st.nextToken());
+
+            for (int j = ldy; j < ruy; j++) {
+                for (int k = ldx; k < rux; k++) {
+                    arr[M-1-j][k] = 1; //그림이랑 똑같이 넣기
                 }
             }
         }
-        System.out.println(ans);
-        arrayList.sort(Comparator.naturalOrder());
-        for (int num : arrayList) {
-            System.out.print(num + " ");
+
+        int count = 0; //영역 수
+        List<Integer> areas = new ArrayList<>();
+        //영역의 수와 넓이 구하는 로직
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if(arr[i][j] != 0) continue; //벽이면 continue
+                if(vis[i][j] == 1) continue; //방문했으면 continue
+                count++;
+                //dfs 를 돌면서
+                areas.add(dfs(i, j, N, M));
+            }
+        }
+
+        //출력
+        System.out.println(count);
+        //areas 정렬
+        Collections.sort(areas);
+        for (int area : areas) {
+            System.out.print(area + " ");
         }
     }//main
 
-    static int[] dy = {-1, 0, 1, 0};
-    static int[] dx = {0, 1, 0, -1};
-    static int size = 0;
-    static void dfs(int y, int x, int m, int n) {
-        vis[y][x] = 1;
-        size++;
+    static public int dfs(int y, int x, int n, int m) {
+        vis[y][x] = 1; //방문 먼저 하기
+        int area = 1;
         for (int i = 0; i < 4; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
-            if (ny < 0 || nx < 0 || ny >= m || nx >= n) continue;
-            if(arr[ny][nx] != 0 || vis[ny][nx] == 1) continue;
-            dfs(ny, nx, m, n);
+            if (ny < 0 || nx < 0 || ny >= m || nx >= n) {
+                continue;
+            }
+            if (arr[ny][nx] == 1 || vis[ny][nx] == 1) {
+                continue;
+            }//벽이거나 방문했으면 패스
+
+            area += dfs(ny, nx, n, m);
         }
+        return area;
     }
 }
