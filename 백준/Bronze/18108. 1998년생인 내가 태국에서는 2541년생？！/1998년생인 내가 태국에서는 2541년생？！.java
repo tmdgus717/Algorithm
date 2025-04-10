2f2409fe4c9b4f1508e74af5f43932dc53a62f62
SeungHyun <67327887+tmdgus717@users.noreply.github.com>
2025-04-10 13:49:04 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 172 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,13 @@
+/* Online Java Compiler and Editor */
+// tip: each public class is put in its own file
+import java.util.*;
+public class Main
+{
+    // tip: arguments are passed via the field below this editor
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a-543);
+    }
+}
