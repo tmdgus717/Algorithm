da507810b3c34dd115c2d1d06d861ac36890b478
SeungHyun <67327887+tmdgus717@users.noreply.github.com>
2025-04-21 17:41:11 +0900

    [Bronze V] Title: 새싹, Time: 104 ms, Memory: 13940 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("         ,r'\"7\n");
+        System.out.print("r`-_   ,'  ,/\n");
+        System.out.print(" \\. \". L_r'\n");
+        System.out.print("   `~\\/\n");
+        System.out.print("      |\n");
+        System.out.print("      |\n");
+
+    }
+
+}
