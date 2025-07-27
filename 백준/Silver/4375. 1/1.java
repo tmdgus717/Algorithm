42c798fb73f5c90f00c1e4c377243334b885b42c
SeungHyun <67327887+tmdgus717@users.noreply.github.com>
2025-07-27 16:29:45 +0900

    [Silver III] Title: 1, Time: 104 ms, Memory: 14024 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.java
+++ 백준/Silver/4375. 1/1.java
@@ -1,26 +1,28 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-//2와 5로 나누어 떨어지지 않는 정수 n(1 ≤ n ≤ 10000)가 주어졌을 때, 각 자릿수가 모두 1로만 이루어진 n의 배수를 찾는 프로그램을 작성하시오.
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while (true) {
+            s = br.readLine();
+            if (s == null) break;
+            int num = Integer.parseInt(s);
+            int answer = 1;
+            int count = 1;
 
-        Scanner scan = new Scanner(System.in);
-
-        while (scan.hasNextInt()) {
-
-            int num = scan.nextInt();
-            int mod = 0;
-
-            for(int i = 1;;i++){
-                mod = (mod*10+1)%num;
-                if(mod == 0) {
-                    System.out.println(i);
+            while (true){
+                if (answer % num == 0){
+                    System.out.println(count);
                     break;
+                }else {
+                    answer = (answer * 10 + 1) % num;
+                    count++;
                 }
             }
 
-        }
-    }
+        }//while(입력받은 횟수만큼)
+    }//main
+
 }
