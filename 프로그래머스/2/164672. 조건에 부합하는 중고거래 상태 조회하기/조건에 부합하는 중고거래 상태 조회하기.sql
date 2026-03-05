30769731522820003ea032913b2752b91cc7e8d8
SeungHyun <67327887+tmdgus717@users.noreply.github.com>
2026-03-05 15:11:13 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,14 @@
+-- 코드를 입력하세요
+-- 2022년 10월 5일에 등록된
+SELECT BOARD_ID, WRITER_ID, TITLE, PRICE,
+    CASE
+        WHEN UGB.STATUS = "DONE" 
+        THEN '거래완료'
+        WHEN UGB.STATUS = "SALE"
+        THEN '판매중'
+        WHEN UGB.STATUS = "RESERVED"
+        THEN '예약중'
+        END AS STATUS
+FROM USED_GOODS_BOARD AS UGB
+WHERE CREATED_DATE = '2022-10-05'
+ORDER BY BOARD_ID DESC
